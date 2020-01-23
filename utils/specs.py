17bc47829b8f18c4bb5529d66d88c633f8003ce0
ArtrixTech <Artrix@artrix.tech>
2020-01-23 23:26:36 +0800

    [Front] first step

--- utils/specs.py
+++ utils/specs.py
@@ -3,4 +3,4 @@ HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
 
 # Frontend
-DOMAIN = '2020.rapi.link'
+DOMAIN = 'p.rapi.link'
